b5ac6a66bd4e8de7c4c3b3dc206f61cf938f4727
Simon <nitrologic@gmail.com>
2016-07-20 12:58:57 +1200

    bump

--- opencv/showimage.cpp
+++ opencv/showimage.cpp
@@ -1,39 +1,12 @@
-#include "opencv2/opencv.hpp"
-
-using namespace cv;
-
-int main(int, char**)
-{
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-
-    Mat edges;
-    namedWindow("edges",1);
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-
-printf("thanks\n");
-    return 0;
-}
-/*
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
+
+using namespace cv;
 using namespace std;
 
-
-int imagemain( int argc, char** argv )
+int main( int argc, char** argv )
 {
     if( argc != 2)
     {
@@ -56,4 +29,3 @@ int imagemain( int argc, char** argv )
     waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
-*/
