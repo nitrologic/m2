76816685c25ddf833976ffd10c2439c7e60eaec4
Simon <nitrologic@gmail.com>
2016-07-31 12:48:17 +1200

    bump fb

--- linux/fb/fb.h
+++ linux/fb/fb.h
@@ -9,108 +9,45 @@
 #include <linux/fb.h>
 #include <string.h>
 
+class fb{
 
-int testfb(){
+public:
 
-	char filename[128];
-	fb_fix_screeninfo fix[2]={0};
-	fb_var_screeninfo var[2]={0};
+static int queryFramebuffer(int fd,int *width,int *height,int *depth,int *bytesize){
 
-	for(int i=0;i<2;i++){
-		sprintf(filename, "/dev/fb%d", i);
-		int fb = open(filename, O_RDWR);
-		if (fb==-1){
-			printf("Failed to open device %s\n",filename);
-			continue;
-		}
+	fb_fix_screeninfo fix={0};
+	fb_var_screeninfo var={0};
 
-		if (ioctl(fb,FBIOGET_FSCREENINFO,&fix[i])==-1){
-			printf("Error reading fixed information\n");
-			return 2;
-		}
-
-		if (ioctl(fb,FBIOGET_VSCREENINFO,&var[i])==-1){
-			printf("Error reading fixed information\n");
-			return 2;
-		}
-
-		int w=var[i].xres;
-		int h=var[i].yres;
-		int bpp=var[i].bits_per_pixel;
-		long int n=w*h*bpp/8;
-
-		printf("fix fb%d %s caps=%d\n",i,fix[i].id,fix[i].capabilities);
-		printf("var fb%d %d x %d x %d\n",i,w,h,bpp);
-
-//		printf("var      %dx%d\n",var[i].xres_virtual,var[i].yres_virtual);
-
-
-/*
-//		void *p=mmap(0,n,PROT_READ|PROT_WRITE,MAP_SHARED,fb,0);
-		void *p=mmap(0,n,PROT_WRITE,MAP_SHARED,fb,0);
-
-		if ((int)p==-1) {
-			printf("Error: failed to map framebuffer device to memory");
-		}else{
-			printf("The framebuffer device was mapped to memory successfully.\n");
-			short *s=(short*)p;
-			int dx,dy;
-			int count=1000;
-			while (count--){
-				dx++;
-				for(int y=0;y<h;y++){
-					for(int x=0;x<w;x++){
-						int tx=dx+x;
-						int ty=dy+y;
-						s[x+y*w]=tx*tx*ty*ty;
-					}
-				}
-			}
-			munmap(p,n);
-		}
-*/
-
-		close(fb);
+	if (ioctl(fd,FBIOGET_FSCREENINFO,&fix)==-1){
+		printf("Error reading fixed information\n");
+		return -1;
 	}
+
+	if (ioctl(fd,FBIOGET_VSCREENINFO,&var)==-1){
+		printf("Error reading fixed information\n");
+		return -2;
+	}
+
+	*width=var.xres;
+	*height=var.yres;
+	*depth=var.bits_per_pixel;
+	*bytesize=(*width)*(*height)*(*depth)/8;
 
 	return 0;
 }
 
-/*
-
-const char *hatname="Raspberry Pi Sense HAT Joystick";
-
-int main(){
-	char filename[128];
-	char devicename[512];
-
-	int hat=-1;
-
-	for(int i=0;i<4;i++){
-		sprintf(filename,"/sys/class/input/event%d/device/name",i);
-		int eb=open(filename,O_RDONLY);
-		if(eb==-1) continue;
-		ssize_t n=read(eb,devicename,512);
-		ssize_t nn=strlen(hatname);
-		if(memcmp(hatname,devicename,nn)==0){
-			devicename[n]=0;
-			sprintf(filename,"/dev/input/event%d",i);
-			hat=open(filename,O_RDONLY);
-			printf("%s open %d\n",devicename,hat);
-			break;
-		}
-		close(eb);
-		eb=-1;
+static void *mapFramebuffer(int fd,int sizeBytes){
+	void *memory=mmap(0,sizeBytes,PROT_WRITE,MAP_SHARED,fd,0);
+	if ((int)memory==-1) {
+		printf("Error: mmap failed on fd\n");
+		return nullptr;
 	}
+	printf("Framebuffer mapped to %p\n",memory);
+	return memory;
+}
 
-	while (1){
-		char ev[5];
-		ssize_t n=read(hat,ev,5);
-		if(n==-1) break;
-		if(n){
-			printf("[%d] %d %d  %d  %d %d\n",n,ev[0],ev[1],ev[2],ev[3],ev[4]);
-		}
-	}
+static void unmapFrameBuffer(void *memory,int sizeBytes){
+	munmap(memory,sizeBytes);
+}
 
-*/
-
+};
--- linux/fb/fb.monkey2
+++ linux/fb/fb.monkey2
@@ -6,38 +6,96 @@ Using std..
 
 extern 
 
-Function testfb:int()
+Function open:Int( path:CString,mode:Int )
+Function close:Int( fd:int)
+
+Class fb
+	Function queryFramebuffer(descriptor:Int,width:Int ptr,height:Int ptr,depth:Int ptr,bytecount:Int Ptr)
+	Function mapFramebuffer:Void Ptr(descriptor:Int,bytecount:Int)
+	Function unmapFrameBuffer(memory:Void ptr,bytecount:Int)
+end
 
 Public
 
-Class FrameBuffer16 Extends Pixmap
+Class PixelMap
+	Field width:Int
+	Field height:Int
+	Field data:Short Ptr
+	Field bytecount:Int
+	Field wspan:int
 
-	Method New(w:Int,h:Int,pitch:Int,data:Short Ptr)
-		Super.New(w,h,PixelFormat.IA16,UByte Ptr(data),pitch)
+	Method New(w:Int,h:Int,m:Short Ptr,n:Int)
+		width=w
+		height=h
+		data=m
+		bytecount=n
+		wspan=bytecount/(2*height)
 	End
 	
-	Method Copy(source:Pixmap,x:Int,y:Int)
+	Method Plot(x:int,y:int,c:Short)
+		data[y*wspan+x]=c
+	End
+	
+	Method Box(x:Int,y:Int,w:Int,h:Int,c:Short)
+		For Local i:=0 To w
+			Plot(x+i,y,c)
+			Plot(x+i,y+h,c)
+		Next
+		
+		For Local i:=1 Until h
+			Plot(x,y+i,c)
+			Plot(x+w,y+i,c)
+		next
 	end
+end
 
+Class FrameBuffer16 Extends PixelMap
+	Method New(w:Int,h:Int,data:Void Ptr,bytecount:int)
+		Super.New(w,h,Short Ptr(data),bytecount)				
+		Plot(0,0,$f800)
+		Box(1,1,5,4,$07f0)
+	End	
 End
 
-Function EnumerateFramebuffers()
-	For Local i:=0 Until 16
-		Local dev:="/dev/fb/"+i
-		Local file:=fopen(dev,"r")
-		If file
-			Print "Found "+dev
-			fclose(file)
-		endif
-	next
+Class LinuxHost
+
+	Field fb16:=New Stack<FrameBuffer16>
+
+	Method EnumerateFramebuffers()
+		For Local i:=0 Until 16
+			Local dev:="/dev/fb"+i
+			
+			Local fd:=open(dev,2)
+			
+			If fd<>-1
+
+				Local w:Int,h:int,bpp:int,count:Int
+	
+				fb.queryFramebuffer(fd,Varptr w,Varptr h,Varptr bpp,Varptr count)
+
+				Print "Found "+dev+" "+w+"x"+h+" "+bpp+":bpp count="+count
+				
+				If bpp=16 
+					Local memory:=fb.mapFramebuffer(fd,count)
+					If memory
+						fb16.Push(New FrameBuffer16(w,h,memory,count))
+					Endif
+				Endif
+
+				close(fd)
+			Endif
+		Next	
+		Print "fb16.Count="+fb16.Length
+	end
 end
 
 
 function Main()
+	Local host:=New LinuxHost
 	
 	print "Enumarating framebuffer devices."
 
-	EnumerateFramebuffers()
+	host.EnumerateFramebuffers()
 
-	Print "testfb:"+testfb()
+	
 end
